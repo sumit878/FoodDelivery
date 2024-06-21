f2329df5bb8a7d2c4805ecc83a7424f6fc0d5f85
Skv Sumit <60491961+sumit878@users.noreply.github.com>
2024-06-21 15:06:44 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,4 +14,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'https://fooddelivery-backend-b6q3.onrender.com'
+export const url = 'https://fooddelivery-backend-0g7o.onrender.com'
