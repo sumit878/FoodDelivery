47dad45270fc77571c31b316f9762c721c1462d6
Skv Sumit <60491961+sumit878@users.noreply.github.com>
2024-06-21 19:13:18 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 //placing order from frontend
 const placeOrder = async (req,res) => {
-  const frontend_url = "https://fooddelivery-frontend-92vr.onrender.com";
+  const frontend_url = ”https://fooddelivery-6p52.onrender.com";
   try{
     const newOrder = new orderModel({
       userId:req.body.userId,
